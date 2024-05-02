5e7a45678a86c1c5558ee88bf4c17e88f2f4b045
hmzsumon <expresslife555@gmail.com>
2024-05-02 23:57:58 +0600

    make it better

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
 	console.log('Running in development mode');
 } else {
 	//Global-X-api-1901b8e6064a.herokuapp.com/
-	https: baseUrl = 'https://gxness-ea84b2197e3d.herokuapp.com';
+	https: baseUrl = 'https://icm-api-7808882a0bab.herokuapp.com';
 	// Code specific to production mode
 	console.log('Running in production mode');
 }
