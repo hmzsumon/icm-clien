0b14070458aeca7880baac8fdc3cb1b9fad477a2
hmzsumon <expresslife555@gmail.com>
2024-05-17 21:31:37 +0600

    first commit

--- app/(auth)/rank-and-reward/page.tsx
+++ app/(auth)/rank-and-reward/page.tsx
@@ -5,6 +5,7 @@ const RankAndReward = () => {
 		<div className='p-4'>
 			<h1 className=' text-center text-xl font-semibold text-gray-800'>
 				Rank and Reward
+				<h2 className='mt-10 text-2xl font-bold'>Coming soon</h2>
 			</h1>
 		</div>
 	);
