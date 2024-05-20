877981d1cc4f2aa8b1b04b5eaf50b071ff8f60e3
hmzsumon <expresslife555@gmail.com>
2024-05-20 15:17:01 +0600

    02 commit

--- components/Settings/TopCard.tsx
+++ components/Settings/TopCard.tsx
@@ -4,6 +4,7 @@ import { RxAvatar } from 'react-icons/rx';
 import { Tooltip } from 'react-tooltip';
 import Link from 'next/link';
 import { useSelector } from 'react-redux';
+import { MdOutlineVerified } from 'react-icons/md';
 
 const TopCard = () => {
 	const { user } = useSelector((state: any) => state.auth);
@@ -11,35 +12,64 @@ const TopCard = () => {
 		<div className=' '>
 			<div className='grid grid-cols-1 md:grid-cols-2 gap-5 py-2 '>
 				{/* Verification */}
-				<Card className=''>
-					<div className='flex justify-between items-center'>
-						<div className='flex items-center gap-3'>
-							<span className='text-2xl p-2  border-4 rounded-full text-black'>
-								<RxAvatar />
-							</span>
-							<div className='space-y-1'>
-								<p className='text-[12px] font-medium text-primary'>
-									Verification status
-								</p>
-								<p className='text-[#C0424D] text-lg font-semibold'>
-									Not verified
-								</p>
-								<p className='text-[12px]'>0/1 steps complete</p>
+				{user?.kyc_verified ? (
+					<Card className=''>
+						<div className='flex justify-between items-center'>
+							<div className='flex items-center gap-3'>
+								<span className='text-2xl p-2  border-4 rounded-full text-black'>
+									<RxAvatar />
+								</span>
+								<div className='space-y-1'>
+									<p className='text-[12px] font-medium text-primary'>
+										Verification status
+									</p>
+									<p className='text-icm-green flex items-center gap-1  font-semibold'>
+										<MdOutlineVerified className='text-xl' /> Verified
+									</p>
+								</div>
 							</div>
-						</div>
-						{user?.kyc_step === 4 ? (
-							<button className='rounded py-2 px-4 text-sm font-semibold  bg-orange-400 text-slate-700 duration-300 hover:bg-orange-300'>
-								Under Review
+
+							<button
+								className='rounded py-2 px-4 text-sm font-semibold bg-green-500  text-slate-700 duration-300 '
+								disabled
+							>
+								Complete
 							</button>
-						) : (
-							<Link href='/verification'>
-								<button className='rounded py-2 px-4 text-sm font-semibold bg-green-500  text-slate-700 duration-300 hover:bg-icm-green'>
-									Complete Now
+						</div>
+					</Card>
+				) : (
+					<Card className=''>
+						<div className='flex justify-between items-center'>
+							<div className='flex items-center gap-3'>
+								<span className='text-2xl p-2  border-4 rounded-full text-black'>
+									<RxAvatar />
+								</span>
+								<div className='space-y-1'>
+									<p className='text-[12px] font-medium text-primary'>
+										Verification status
+									</p>
+									<p className='text-[#C0424D] text-lg font-semibold'>
+										Not verified
+									</p>
+									<p className='text-[12px]'>
+										{user?.kyc_step}/4 steps complete
+									</p>
+								</div>
+							</div>
+							{user?.kyc_step === 4 ? (
+								<button className='rounded py-2 px-4 text-sm font-semibold  bg-orange-400 text-slate-700 duration-300 hover:bg-orange-300'>
+									Under Review
 								</button>
-							</Link>
-						)}
-					</div>
-				</Card>
+							) : (
+								<Link href='/verification'>
+									<button className='rounded py-2 px-4 text-sm font-semibold bg-green-500  text-slate-700 duration-300 hover:bg-icm-green'>
+										Complete Now
+									</button>
+								</Link>
+							)}
+						</div>
+					</Card>
+				)}
 				{/* balance */}
 				<Card>
 					<div className='flex justify-between items-center'>
