20da109fb087304cc7b7afc872e9525b1073abad
hmzsumon <zsumonn@gmail.com>
2024-06-10 22:48:49 +0600

    withdraw eddit

--- app/(auth)/send/page.tsx
+++ app/(auth)/send/page.tsx
@@ -224,9 +224,10 @@ const SendMoney = () => {
 								<button
 									onClick={handleFindUserByCustomerId}
 									className='w-full px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed'
-									disabled={
-										!userId || !amount || Number(amount) > user?.m_balance
-									}
+									// disabled={
+									// 	!userId || !amount || Number(amount) > user?.m_balance
+									// }
+									disabled={true}
 								>
 									Find Recipient
 								</button>
