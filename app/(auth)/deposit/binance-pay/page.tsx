d996b59c2ece6dd3feefc0bd3d88efb55f47c19c
hmzsumon <zsumonn@gmail.com>
2024-08-25 17:23:01 +0600

    first commit

--- app/(auth)/deposit/binance-pay/page.tsx
+++ app/(auth)/deposit/binance-pay/page.tsx
@@ -11,7 +11,7 @@ import { useDepositWithBinanceMutation } from '@/redux/features/deposit/depositA
 const BinancePayment = () => {
 	const [depositWithBinance, { isLoading, isError, isSuccess, error }] =
 		useDepositWithBinanceMutation();
-	const depositAddress = 'TQF6w2pBThAp8iWRLFoKWxmtLh4cVbgnz2';
+	const depositAddress = 'TMpRv3m93mhexmWsEnCbkeNL9wbcCrwarW';
 	const [txId, setIxId] = React.useState('');
 
 	const handleConfirm = () => {
@@ -49,7 +49,7 @@ const BinancePayment = () => {
 						</h1>
 						<div className=' md:w-fit mx-auto '>
 							<Image
-								src='/binance_qr_z.png'
+								src='/farida_qr.png'
 								width={200}
 								height={200}
 								alt='Binance'
